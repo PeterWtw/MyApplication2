eaa4944d916b1a30e63e34052315f5b882e2e064
PeterWtw <1982078787@qq.com>
2016-08-09 17:33:31 +0800

    第五次

--- app/src/main/java/com/example/wtw/myapplication2/startActivity.java
+++ app/src/main/java/com/example/wtw/myapplication2/startActivity.java
@@ -1,7 +1,6 @@
 package com.example.wtw.myapplication2;
 
 import android.app.Activity;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,13 +9,15 @@ import android.view.WindowManager;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Created by WTW on 2016/8/9.
+ */
 
-public class MainActivity extends Activity {
-
+public class startActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_main);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -24,19 +25,5 @@ public class MainActivity extends Activity {
         }
 
 
-        final Intent in=new Intent();
-        Timer timer=new Timer();
-        TimerTask task=new TimerTask() {
-            @Override
-            public void run() {
-                in.setClass(MainActivity.this,startActivity.class);
-                startActivity(in);
-                finish();
-            }
-        };
-        timer.schedule(task,1000*3);
-
     }
-
-
 }
