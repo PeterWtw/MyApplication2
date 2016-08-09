e0ac37acb69f59f898e5f30c6a7a9be0866ea410
PeterWtw <1982078787@qq.com>
2016-08-09 10:00:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wtw/myapplication2/adapter/aaa.java
@@ -0,0 +1,8 @@
+package com.example.wtw.myapplication2.adapter;
+
+/**
+ * Created by WTW on 2016/8/9.
+ */
+
+public class aaa {
+}
---
+++ app/src/main/java/com/example/wtw/myapplication2/entity/bbb.java
@@ -0,0 +1,8 @@
+package com.example.wtw.myapplication2.entity;
+
+/**
+ * Created by WTW on 2016/8/9.
+ */
+
+public class bbb {
+}
